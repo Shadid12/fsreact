55fd6acf0b459ebdf58b35fb96eb78264cd126bf
shadid12 <shadidhaque2014@gmail.com>
2017-09-16 23:33:04 -0400

    some message is awesome

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res){
+  res.send('good vibes')
 })
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
+io.on('connection', function(socket){
+  console.log('a user connected')
+  socket.on('message', function(msg){
+  	console.log(msg)
+  })
+})
+
+http.listen(3001, function(){
+  console.log('listening on *:3001')
 })
--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { socketConnect } from 'socket.io-react'
+import SocketIOClient from 'socket.io-client'
 
 class ChatRoom extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			msg: ''
+		}
+		
+		this.socket = SocketIOClient('http://localhost:3001/')
+		this.handleChange = this.handleChange.bind(this);
+    	this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange = (e) => {
+		this.setState({value: e.target.value})
+	}
+
+	handleSubmit = (e) => {
+		console.log(this.state.value)
+		this.socket.emit('message', this.state.value)
+		this.setState({value: ''})
+		e.preventDefault()
+	}
+
 	render() {
 		return(
 			<div>
 				<ul id="messages"></ul>
-			    <form action="">
-			      <input id="m" autoComplete="off" /><button>Send</button>
-			    </form>
+			      <input id="m" autoComplete="off" value={this.state.value} onChange={this.handleChange} />
+			      <button onClick={this.handleSubmit}>Send </button>
 		    </div>
 		)
 	}
 }
 
-export default socketConnect(ChatRoom)
+export default ChatRoom
