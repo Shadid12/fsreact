7de3f71aa8ef140663e7e100bc644edba21bd9e3
shadid12 <shadidhaque2014@gmail.com>
2017-09-16 21:01:18 -0400

    api added

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import { socketConnect } from 'socket.io-react'
 
 class ChatRoom extends React.Component {
 	render() {
 		return(
 			<div>
-			<ul id="messages"></ul>
-		    <form action="">
-		      <input id="m" autocomplete="off" /><button>Send</button>
-		    </form>
+				<ul id="messages"></ul>
+			    <form action="">
+			      <input id="m" autoComplete="off" /><button>Send</button>
+			    </form>
 		    </div>
 		)
 	}
 }
 
-export default ChatRoom
+export default socketConnect(ChatRoom)
