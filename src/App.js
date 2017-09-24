528096cd1ac095013f713bc70be40995bc44e19c
shadid12 <shadidhaque2012@gmail.com>
2017-09-24 11:45:30 -0400

    new login component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
 import ChatRoom from './components/ChatRoom'
+import LogIn from './components/LogIn'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ChatRoom />
+      	<Router>
+	      	<div>
+	      		<ul>
+			        <li><Link to="/login">Login</Link></li>
+			    </ul>
+	      		<ChatRoom />
+	      		<Route exact path="/login" component={LogIn}/>
+	      	</div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class LogIn extends React.Component {
+	render(){
+		return(
+			<h1>Hey Whats going on</h1>
+		)
+	}
+}
+
+export default LogIn
